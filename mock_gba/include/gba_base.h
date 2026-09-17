60eb398745a1aa1f72e1bc2a767d255685963ffb
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-09-17 13:11:35 +0200

    mock: include video/input/syscalls in gba_base.h

--- mock_gba/include/gba_base.h
+++ mock_gba/include/gba_base.h
@@ -1,6 +1,10 @@
 #ifndef MOCK_GBA_BASE_H
 #define MOCK_GBA_BASE_H
 
+#include "gba_video.h"
+#include "gba_input.h"
+#include "gba_systemcalls.h"
+
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
