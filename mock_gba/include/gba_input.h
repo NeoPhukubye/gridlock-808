9fdb91f299cec7db79001f2ea6721e83723944de
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-09-17 13:11:41 +0200

    mock: add KEY bitmasks and keysHeld declaration

--- mock_gba/include/gba_input.h
+++ mock_gba/include/gba_input.h
@@ -1,7 +1,19 @@
 #ifndef MOCK_GBA_INPUT_H
 #define MOCK_GBA_INPUT_H
 
+#define KEY_A      1
+#define KEY_B      2
+#define KEY_SELECT 4
+#define KEY_START  8
+#define KEY_RIGHT  16
+#define KEY_LEFT   32
+#define KEY_UP     64
+#define KEY_DOWN   128
+#define KEY_R      256
+#define KEY_L      512
+
 void scanKeys();
 unsigned short keysDown();
+unsigned short keysHeld();
 
 #endif // MOCK_GBA_INPUT_H
