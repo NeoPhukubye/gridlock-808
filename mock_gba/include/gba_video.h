d6f89fd2d6704eea9e9e52a90c6a852ca5ebb5bb
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-09-17 13:11:57 +0200

    mock: declare MEM_VRAM array, drop duplicate m3_plot

--- mock_gba/include/gba_video.h
+++ mock_gba/include/gba_video.h
@@ -5,11 +5,8 @@
 #define BG2_ENABLE 0x0400
 
 extern volatile unsigned short REG_DISPCNT;
+extern volatile unsigned short MEM_VRAM[240 * 160];
 
 #define RGB5(r,g,b) (((r) & 31) | (((g) & 31) << 5) | (((b) & 31) << 10))
-
-inline void m3_plot(int x, int y, unsigned short color) {
-    // Mock pixel plot - headless no-op for tests
-}
 
 #endif // MOCK_GBA_VIDEO_H
